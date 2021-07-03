33a3ad20c95dbc2c6e6f18d2da3b12affc456cb8
Piotr Furmanek <piotrfurmanek@yahoo.co.uk>
2021-07-03 19:19:56 +0100

    New conn details

--- include/connect.php
+++ include/connect.php
@@ -1,9 +1,9 @@
 <?php
 	   $dbhost = 'pete85com1.ipagemysql.com';
 	   $dbuser = 'pete85';
-	   $dbpass = 'Vectr@19CDti';
+	   $dbpass = 'T@deusz1306';
 	   
-	   $conn = new mysqli($dbhost, $dbuser, $dbpass, "pete85portfolio");
+	   $conn = new mysqli($dbhost, $dbuser, $dbpass, "trulysti_pete85portfolio");
 
 	   if ($conn->connect_error) {
            echo "Error: Unable to connect to MySQL.";
